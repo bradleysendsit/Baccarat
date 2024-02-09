348f1ab291fcea10300baa020f86855805be1a2e
bradleysendsit <159570590+bradleysendsit@users.noreply.github.com>
2024-02-09 13:17:40 -0500

    script.js

--- script.js
+++ script.js
@@ -53,6 +53,7 @@ function displayCards(elementId, hand) {
     });
 }
 
+
 function determineWinner() {
     const playerTotal = calculateTotal(playerHand);
     const bankerTotal = calculateTotal(bankerHand);
